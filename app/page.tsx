0f3fd9f57ee2ad760827ba4a603f5a5cc4caf3d1
Atsu <122103592+Atsu927@users.noreply.github.com>
2024-11-25 22:42:00 +0400

    setup
    setting up metadata and dev builds

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-3xl">
+                SkyBox - Your secure cloud for easy file storage and sharing,
+                accessible anytime, anywhere.
+            </h1>
+        </div>
+    )
+}
