9dbac9c4be67f25f469acee586dd96f510448ace
Atsu <122103592+Atsu927@users.noreply.github.com>
2024-12-13 18:31:04 +0400

    delete-files
    implemented where users can delete files

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -24,7 +24,11 @@ import Link from 'next/link'
 import { constructDownloadUrl } from '@/lib/utils'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
-import { renameFile, updateFileUsers } from '@/lib/actions/file.actions'
+import {
+    deleteFile,
+    renameFile,
+    updateFileUsers,
+} from '@/lib/actions/file.actions'
 import { usePathname } from 'next/navigation'
 import { FileDetails, ShareInput } from './ActionsModalContent'
 
@@ -65,7 +69,12 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
                     emails,
                     path,
                 }),
-            delete: () => console.log('delete'),
+            delete: () =>
+                deleteFile({
+                    fileId: file.$id,
+                    path,
+                    bucketFileId: file.bucketFileId,
+                }),
         }
 
         success = await actions[action.value as keyof typeof actions]()
@@ -111,6 +120,15 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
                             onInputChange={setEmails}
                             onRemove={handleRemoveUser}
                         />
+                    )}
+                    {value === 'delete' && (
+                        <p className="delete-confirmation">
+                            Are you sure you want to delete?{' '}
+                            <span className="delete-file-name">
+                                {file.name}
+                            </span>
+                            ?
+                        </p>
                     )}
                 </DialogHeader>
                 {['rename', 'delete', 'share'].includes(value) && (
